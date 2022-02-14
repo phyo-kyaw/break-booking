34f2a45bb7996f6aaf88207d8fdb7c07037da9c6
phyo-kyaw <phy0ky6w@gmail.com>
2022-02-14 16:11:41 +1100

    kafka id

--- src/main/java/com/booking/event/BookedNotificationEvent.java
+++ src/main/java/com/booking/event/BookedNotificationEvent.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.util.Date;
 
 @Data
@@ -12,6 +15,9 @@ import java.util.Date;
 @AllArgsConstructor
 @SuperBuilder
 public class BookedNotificationEvent extends BaseEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
     private String bookerEmail;
     private String bookerName;
 }
